9ea99f7ed4a64e96a2d54c4e65a374e56070506c
Osama Chelhaoui <dooomdomino@gmail.com>
2023-05-17 18:43:06 +0200

    Inizializzazione classi progetto
    close #5

---
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -0,0 +1,14 @@
+////////////////////////////////////////////////////////////////////
+// Osama Chelhaoui 2042333
+// Giovanni Ponso 2000558
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+public class IntegerToRoman {
+
+    public static String convert(int number){
+        // TODO
+        return null;
+    }
+}
