7eabdaee8f402ab2ae196315bd0b9b0ea41ee7f1
Osama Chelhaoui <dooomdomino@gmail.com>
2023-05-17 21:51:47 +0200

    Implementazione IntegerToRoman
    close #7

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -5,10 +5,32 @@
 
 package it.unipd.mtss;
 
+import java.util.TreeMap;
+
 public class IntegerToRoman {
 
+    private static final TreeMap<Integer, String> map = new TreeMap<>();
+    static {
+        map.put(1000, "M");
+        map.put(900, "CM");
+        map.put(500, "D");
+        map.put(400, "CD");
+        map.put(100, "C");
+        map.put(90, "XC");
+        map.put(50, "L");
+        map.put(40, "XL");
+        map.put(10, "X");
+        map.put(9, "IX");
+        map.put(5, "V");
+        map.put(4, "IV");
+        map.put(1, "I");
+    }
+
     public static String convert(int number){
-        // TODO
-        return null;
+        int roman = map.floorKey(number);
+        if (number == roman) {
+            return map.get(number);
+        }
+        return map.get(roman) + convert(number - roman);
     }
 }
